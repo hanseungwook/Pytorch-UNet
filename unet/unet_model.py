58df253944031286147bb64b35f879a0f9bbe056
Seungwook Han <sh3264@coumbia.edu>
2020-04-23 19:34:23 -0400

    Implemented variable # of tails for 128 and 256 mask networks

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -112,6 +112,86 @@ class UNet_Sigmoid_3Tail(nn.Module):
 
         return torch.cat((x_out1, x_out2, x_out3), dim=1)
 
+class UNet_Sigmoid_NTail_128(nn.Module):
+    def __init__(self, n_channels, n_classes, n_tails=3, bilinear=True):
+        super(UNet_Sigmoid_NTail_128, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+        self.sigmoid = nn.Sigmoid()
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 32, bilinear)
+        self.outc_modules = nn.ModuleList()
+        for i in range(n_tails):
+            self.outc_modules.append(OutConv(32, n_classes))
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x_out = torch.empty(0)
+
+        for layer in self.outc_modules:
+            cur_x_out = layer(x)
+            x_out = torch.cat((x_out, cur_x_out), dim=1)
+        
+        return x_out
+
+class UNet_Sigmoid_NTail_256(nn.Module):
+    def __init__(self, n_channels, n_classes, n_tails=3, bilinear=True):
+        super(UNet_Sigmoid_NTail_256, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+        self.sigmoid = nn.Sigmoid()
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 16, bilinear)
+        self.outc_modules = nn.ModuleList()
+        for i in range(n_tails):
+            self.outc_modules.append(OutConv(16, n_classes))
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x_out = torch.empty(0)
+
+        for layer in self.outc_modules:
+            cur_x_out = layer(x)
+            x_out = torch.cat((x_out, cur_x_out), dim=1)
+        
+        return x_out
+
 class UNet_Small(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=True):
         super(UNet_Small, self).__init__()
