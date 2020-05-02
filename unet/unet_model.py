daff2bcc184a3c74c0d3ecb64e70e5d363dfbae3
Seungwook Han <sh3264@coumbia.edu>
2020-05-02 13:44:02 -0400

    Added bigger feature space ver of unet

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -40,6 +40,39 @@ class UNet(nn.Module):
 
         return logits
 
+class UNet1(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(UNet1, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 128)
+        self.down1 = Down(128, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 128, bilinear)
+        self.outc = OutConv(128, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+
+        return logits
+
 class UNet_Sigmoid(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=True):
         super(UNet_Sigmoid, self).__init__()
